8afbb86ccbd53c5fef95797eef9d51c90ddb2dc7
Pim <Pimvgd@gmail.com>
2017-04-11 01:28:30 +0200

    add missing space between lines for breast smother

--- NightgamesMod/nightgames/skills/BreastSmother.java
+++ NightgamesMod/nightgames/skills/BreastSmother.java
@@ -123,7 +123,7 @@ public class BreastSmother extends Skill {
         }
         else {
             b.append( "You rock " + target.getName() + "'s head between your "
-                            + getSelf().body.getRandomBreasts().fullDescribe(getSelf()) + " trying to force " + target.directObject() + " to gasp.");                           
+                            + getSelf().body.getRandomBreasts().fullDescribe(getSelf()) + " trying to force " + target.directObject() + " to gasp. ");                           
         }
         
         if (getSelf().has(Trait.temptingtits)) {
