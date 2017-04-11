d226c778aa8be6a9a2efe58b9eef0bf5f8255d6d
Pim <Pimvgd@gmail.com>
2017-04-11 02:00:53 +0200

    properly pluralize grind ("You trembles and gasps" -> "you tremble and gasp")

--- NightgamesMod/nightgames/skills/Grind.java
+++ NightgamesMod/nightgames/skills/Grind.java
@@ -92,11 +92,11 @@ public class Grind extends Thrust {
             if (getLabel(c).equals(divineName)) {
                 // TODO divine for fucking someone
                 return Global.format(
-                                "{self:SUBJECT} grind {self:possessive} hips against {other:direct-object} without thrusting. {other:SUBJECT} trembles and gasps as the movement stimulates {other:possessive} clit and the walls of {other:possessive} {other:body-part:pussy}.",
+                                "{self:SUBJECT} grind {self:possessive} hips against {other:direct-object} without thrusting. {other:SUBJECT-ACTION:tremble and gasp|trembles and gasps} as the movement stimulates {other:possessive} clit and the walls of {other:possessive} {other:body-part:pussy}.",
                                 getSelf(), target);
             }
             return Global.format(
-                            "{self:SUBJECT} grind {self:possessive} hips against {other:direct-object} without thrusting. {other:SUBJECT} trembles and gasps as the movement stimulates {other:possessive} clit and the walls of {other:possessive} {other:body-part:pussy}.",
+                            "{self:SUBJECT} grind {self:possessive} hips against {other:direct-object} without thrusting. {other:SUBJECT-ACTION:tremble and gasp|trembles and gasps} as the movement stimulates {other:possessive} clit and the walls of {other:possessive} {other:body-part:pussy}.",
                             getSelf(), target);
         }
     }
